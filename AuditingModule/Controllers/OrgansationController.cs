c6e5462543d06b658a88d29da3c7bd444ea3ebeb
ojeshwini <ojeshwini96@gmail.com>
2018-06-06 10:01:11 +0530

    updated login

---
+++ AuditingModule/Controllers/OrgansationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuditingModule.Controllers
+{
+    public class OrgansationController : Controller
+    {
+        // GET: Organsation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
